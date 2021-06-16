720a80ddfeed9173a54fa153eda59b08b88be362
sajjathossain <painlesssajjad@gmail.com>
2021-06-16 17:31:59 +0600

    Added player change functionalty

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -6,28 +6,30 @@ import styles from './GameBoard.module.css';
 const GameBoard = () => {
   const [cubeData, setCubeData] = useState([])
 
-  const { winner, currentPlayer, boardValues, assignValues, disabled } = useContext(GameData)
+  const { winner, currentPlayer, boardValues, assignValues, loading} = useContext(GameData)
 
   useEffect(() => {
     const getData = async () => {
       setCubeData(boardValues)
-      console.log(boardValues)
+      loading.setIsLoading(false)
+      console.log(boardValues["c0"])
     }
 
     getData()
-  }, [])
+  }, [boardValues, currentPlayer])
   
   return (
+      loading.isLoading ? <h2>Loading...</h2> :
       <div className={styles.container}>
-        <div className={`${styles.gameCube} `} id="0" onClick={assignValues} >{cubeData[0]}</div>
-        <div className={`${styles.gameCube} `} id="1" onClick={assignValues} >{cubeData[1]}</div>
-        <div className={`${styles.gameCube} `} id="2" onClick={assignValues} >{cubeData[2]}</div>
-        <div className={`${styles.gameCube} `} id="3" onClick={assignValues} >{cubeData[3]}</div>
-        <div className={`${styles.gameCube} `} id="4" onClick={assignValues} >{cubeData[4]}</div>
-        <div className={`${styles.gameCube} `} id="5" onClick={assignValues} >{cubeData[5]}</div>
-        <div className={`${styles.gameCube} `} id="6" onClick={assignValues} >{cubeData[6]}</div>
-        <div className={`${styles.gameCube} `} id="7" onClick={assignValues} >{cubeData[7]}</div>
-        <div className={`${styles.gameCube} `} id="8" onClick={assignValues} >{cubeData[8]}</div>
+        <div className={`${styles.gameCube} ${cubeData["c0"].disabled ? styles.disabled : ""}`} id="c0" onClick={assignValues} >{`${cubeData["c0"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c1"].disabled ? styles.disabled : ""}`} id="c1" onClick={assignValues} >{`${cubeData["c1"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c2"].disabled ? styles.disabled : ""}`} id="c2" onClick={assignValues} >{`${cubeData["c2"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c3"].disabled ? styles.disabled : ""}`} id="c3" onClick={assignValues} >{`${cubeData["c3"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c4"].disabled ? styles.disabled : ""}`} id="c4" onClick={assignValues} >{`${cubeData["c4"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c5"].disabled ? styles.disabled : ""}`} id="c5" onClick={assignValues} >{`${cubeData["c5"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c6"].disabled ? styles.disabled : ""}`} id="c6" onClick={assignValues} >{`${cubeData["c6"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c7"].disabled ? styles.disabled : ""}`} id="c7" onClick={assignValues} >{`${cubeData["c7"].player}`}</div>
+        <div className={`${styles.gameCube} ${cubeData["c8"].disabled ? styles.disabled : ""}`} id="c8" onClick={assignValues} >{`${cubeData["c8"].player}`}</div>
       </div>
   )
 }
--- src/contexts/GameData.js
+++ src/contexts/GameData.js
@@ -3,27 +3,64 @@ import React, { createContext, useState } from 'react';
 export const GameData = createContext()
 
 export const DataProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(true)
     const [winnerFound, setWinnerFound] = useState(false)
     const [currentPlayer, setCurrentPlayer] = useState("X")
-    const [disabled, setDisabled] = useState("")
-    const [boardValues, setBoardValues] = useState([ 
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-    ])
+    const [boardValues, setBoardValues] = useState({
+        "c0": {
+            "player": "",
+            "disabled": false
+        },
+        "c1": {
+            "player": "",
+            "disabled": false
+        },
+        "c2": {
+            "player": "",
+            "disabled": false
+        },
+        "c3": {
+            "player": "",
+            "disabled": false
+        },
+        "c4": {
+            "player": "",
+            "disabled": false
+        },
+        "c5": {
+            "player": "",
+            "disabled": false
+        },
+        "c6": {
+            "player": "",
+            "disabled": false
+        },
+        "c7": {
+            "player": "",
+            "disabled": false
+        },
+        "c8": {
+            "player": "",
+            "disabled": false
+        },
+    })
+
+    const checkWinner = () => {
+        if(boardValues["c0"].player === boardValues["c1"].player && boardValues["c1"].player === boardValues["c2"].player){
+            return true
+        }
+
+        return false
+    }
 
     const assignValues = (event) => {
         setCurrentPlayer(currentPlayer === "X" ? "O" : "X")
-        const cubeIndex = parseInt(event.target.id)
-        boardValues[cubeIndex] = currentPlayer
-        setDisabled(`disabled${currentPlayer}`)
-        console.log(cubeIndex, currentPlayer)
+        const cubeIndex = event.target.id
+        boardValues[cubeIndex].player = currentPlayer
+        boardValues[cubeIndex].disabled = true
+        const didWin = checkWinner()
+        console.log(event.target.className)
+        console.log(cubeIndex, currentPlayer, didWin)
     }
 
     const values = {
@@ -31,7 +68,7 @@ export const DataProvider = ({ children }) => {
         currentPlayer,
         boardValues,
         assignValues,
-        disabled
+        loading: { isLoading, setIsLoading }
     }
 
     return (
